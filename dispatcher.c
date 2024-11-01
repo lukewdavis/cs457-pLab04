5453ceff4f5a97e2628b90166f6da9e91d0d3a41
Luke Davis <99206365+lukewdavis@users.noreply.github.com>
2024-10-31 21:33:27 -0400

    variable over deletion

--- dispatcher.c
+++ dispatcher.c
@@ -127,6 +127,7 @@ int main( int argc , char *argv[] )
                 close( BtoA[ READ_END ] ) ;   
 
                 printf("\nDispatcher is now waiting for Amal to terminate\n") ;
+                int exitStatus;
                 waitpid( amalPID , &exitStatus , 0 ) ;
 
                 printf("\nDispatcher is now waiting for Basim to terminate\n") ;
